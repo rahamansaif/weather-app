8c2639d8d2bc682cf0a79beba028ba35bbb584c3
rahamansaif <saifrahaman500@gmail.com>
2024-04-18 14:11:07 +0530

    weather model

--- weather_app/weather/models.py
+++ weather_app/weather/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Weather(models.Model):
+    date = models.DateField()
+    lat = models.FloatField()
+    lon = models.FloatField()
+    city = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    temperatures = models.JSONField()
